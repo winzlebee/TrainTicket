1e0a1ffb4ed6db5c75a6386fce37908a059229da
DrkMatr1984 <Drkmatr1984@aol.com>
2013-11-05 13:56:21 -0500

    Updated Source to Version located on bukkit.org

--- src/main/java/me/wizzledonker/plugins/trainticket/TrainTicketBlockListener.java
+++ src/main/java/me/wizzledonker/plugins/trainticket/TrainTicketBlockListener.java
@@ -1,40 +1,40 @@
-/*
- * The trainticket block listener
- * GPL v2.0 or higher licence. By WizzleDonker @ 29/11/11
- */
-package me.wizzledonker.plugins.trainticket;
+/*    */ package me.wizzledonker.plugins.trainticket;
+/*    */ 
+/*    */ import org.bukkit.ChatColor;
+/*    */ import org.bukkit.entity.Player;
+/*    */ import org.bukkit.event.EventHandler;
+/*    */ import org.bukkit.event.Listener;
+/*    */ import org.bukkit.event.block.SignChangeEvent;
+/*    */ 
+/*    */ public class TrainTicketBlockListener
+/*    */   implements Listener
+/*    */ {
+/*    */   public static Trainticket plugin;
+/*    */ 
+/*    */   public TrainTicketBlockListener(Trainticket instance)
+/*    */   {
+/* 21 */     plugin = instance;
+/*    */   }
+/*    */ 
+/*    */   @EventHandler
+/*    */   public void onSignChange(SignChangeEvent event) {
+/* 26 */     Player player = event.getPlayer();
+/* 27 */     if (!event.getLine(0).equalsIgnoreCase("[booth]")) {
+/* 28 */       return;
+/*    */     }
+/* 30 */     if (!player.hasPermission("TrainTicket.booth.create")) {
+/* 31 */       event.setLine(0, "The booth:");
+/* 32 */       return;
+/*    */     }
+/* 34 */     if ((event.getLine(1).isEmpty()) || (event.getLine(2).isEmpty())) {
+/* 35 */       player.sendMessage(ChatColor.DARK_RED + "Please fill in the missing values!");
+/* 36 */       return;
+/*    */     }
+/* 38 */     event.setLine(0, ChatColor.GOLD + event.getLine(0));
+/*    */   }
+/*    */ }
 
-import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
-import org.bukkit.event.block.BlockListener;
-import org.bukkit.event.block.SignChangeEvent;
-
-/**
- *
- * @author Win
+/* Location:           C:\Users\DrkMatr\Desktop\TrainTicket-1.4.jar
+ * Qualified Name:     me.wizzledonker.plugins.trainticket.TrainTicketBlockListener
+ * JD-Core Version:    0.6.2
  */
-public class TrainTicketBlockListener extends BlockListener {
-    public static Trainticket plugin;
-    
-    public TrainTicketBlockListener(Trainticket instance) {
-        plugin = instance;
-    }
-    
-    @Override
-    public void onSignChange(SignChangeEvent event) {
-        Player player = event.getPlayer();
-        if (!event.getLine(0).equalsIgnoreCase("[booth]")) {
-            return;
-        }
-        if (!player.hasPermission("TrainTicket.booth.create")) {
-            event.setLine(0, "The booth:");
-            return;
-        }
-        if (event.getLine(1).isEmpty() || event.getLine(2).isEmpty()) {
-            player.sendMessage(ChatColor.DARK_RED + "Please fill in the missing values!");
-            return;
-        }
-        event.setLine(0, ChatColor.GOLD + event.getLine(0));
-    }
-    
-}
--- src/main/java/me/wizzledonker/plugins/trainticket/TrainTicketListener.java
+++ src/main/java/me/wizzledonker/plugins/trainticket/TrainTicketListener.java
@@ -1,40 +1,45 @@
-package me.wizzledonker.plugins.trainticket;
+/*    */ package me.wizzledonker.plugins.trainticket;
+/*    */ 
+/*    */ import org.bukkit.entity.Player;
+/*    */ import org.bukkit.event.EventHandler;
+/*    */ import org.bukkit.event.Listener;
+/*    */ import org.bukkit.event.vehicle.VehicleEnterEvent;
+/*    */ import org.bukkit.inventory.ItemStack;
+/*    */ 
+/*    */ public class TrainTicketListener
+/*    */   implements Listener
+/*    */ {
+/*    */   public static Trainticket plugin;
+/*    */ 
+/*    */   public TrainTicketListener(Trainticket instance)
+/*    */   {
+/* 17 */     plugin = instance;
+/*    */   }
+/*    */ 
+/*    */   @EventHandler
+/*    */   public void onVehicleEnter(VehicleEnterEvent event) {
+/* 22 */     if ((event.getEntered() instanceof Player)) {
+/* 23 */       Player player = (Player)event.getEntered();
+/* 24 */       if (player.hasPermission("TrainTicket.exempt")) {
+/* 25 */         return;
+/*    */       }
+/* 27 */       if (player.isInsideVehicle()) {
+/* 28 */         player.sendMessage(plugin.handleMessages(Integer.valueOf(2)));
+/* 29 */         return;
+/*    */       }
+/* 31 */       if ((player.getItemInHand().getTypeId() == plugin.ticketDataValue) && (plugin.hasTicket(player))) {
+/* 32 */         plugin.setTicket(player, false);
+/* 33 */         player.sendMessage(plugin.handleMessages(Integer.valueOf(1)));
+/* 34 */         player.setItemInHand(null);
+/*    */       } else {
+/* 36 */         player.sendMessage(plugin.handleMessages(Integer.valueOf(3)));
+/* 37 */         event.setCancelled(true);
+/*    */       }
+/*    */     }
+/*    */   }
+/*    */ }
 
-import org.bukkit.entity.Player;
-import org.bukkit.event.vehicle.VehicleEnterEvent;
-import org.bukkit.event.vehicle.VehicleListener;
-
-/**
- *
- * @author Win
+/* Location:           C:\Users\DrkMatr\Desktop\TrainTicket-1.4.jar
+ * Qualified Name:     me.wizzledonker.plugins.trainticket.TrainTicketListener
+ * JD-Core Version:    0.6.2
  */
-public class TrainTicketListener extends VehicleListener{
-    
-    public static Trainticket plugin;
-    
-    public TrainTicketListener(Trainticket instance) {
-        plugin = instance;
-    }
-    
-    @Override
-    public void onVehicleEnter(VehicleEnterEvent event) {
-        if (event.getEntered() instanceof Player) {
-            Player player = (Player) event.getEntered();
-            if (player.hasPermission("TrainTicket.exempt")) {
-                return;
-            }
-            if (player.isInsideVehicle()) {
-                player.sendMessage(plugin.handleMessages(2));
-                return;
-            }
-            if (player.getItemInHand().getTypeId() == plugin.ticketDataValue && plugin.hasTicket(player)) {
-                plugin.setTicket(player, false);
-                player.sendMessage(plugin.handleMessages(1));
-                player.setItemInHand(null);
-            } else {
-                player.sendMessage(plugin.handleMessages(3));
-                event.setCancelled(true);
-            }
-        }
-    }
-}
